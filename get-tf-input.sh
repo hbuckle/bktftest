a53e0f8624edce158eca51691bf5059b2bd0a7f0
Henry Buckle <h.buckle@outlook.com>
2017-12-14 10:27:50 +0000

    generate block step

---
+++ get-tf-input.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -eu
+
+echo 'steps:'
+echo '  - block: "Notify"'
+echo '    fields:'
+
+for i in $(cat ./variables.tf.json | jq '.["variable"] | keys[]'); do
+  echo "      - text: $i"
+  echo "        key: $i"
+done
