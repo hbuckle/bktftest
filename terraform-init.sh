8845da2b5a966cbf8db367e970527dc949d42fdc
Henry Buckle <h.buckle@outlook.com>
2017-12-17 16:25:06 +0000

    debug

--- terraform-init.sh
+++ terraform-init.sh
@@ -6,3 +6,4 @@ apk add terraform --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/e
 az login --msi
 response=$(curl http://localhost:50342/oauth2/token --data "resource=https://management.azure.com/" -H Metadata:true -s)
 echo $response | python access-token.py | jq -c '[.]' > $HOME/.azure/accessTokens.json
+cat $HOME/.azure/accessTokens.json
