07ea8658ed0d1933b2ff26d2a74318fafe03d3bb
Henry Buckle <h.buckle@outlook.com>
2017-12-17 13:03:41 +0000

    reinit

--- terraform-plan.sh
+++ terraform-plan.sh
@@ -4,5 +4,6 @@ set -euo pipefail
 
 TOKEN=$(curl http://localhost:50342/oauth2/token --data "resource=https://management.azure.com/" -H Metadata:true -s)
 echo "[$TOKEN]" > $HOME/.azure/accessTokens.json
+terraform init -input=false
 terraform plan -input=false -out=bkplan.tfplan
 buildkite-agent artifact upload bkplan.tfplan
--- terraform-run.sh
+++ terraform-run.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 COMMAND=$(buildkite-agent meta-data get terraform-command)
 TOKEN=$(curl http://localhost:50342/oauth2/token --data "resource=https://management.azure.com/" -H Metadata:true -s)
 echo "[$TOKEN]" > $HOME/.azure/accessTokens.json
+terraform init -input=false
 case $COMMAND in
   "apply")
     buildkite-agent artifact download bkplan.tfplan
