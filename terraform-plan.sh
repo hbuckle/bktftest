eefc0d3f96449080e010704279aefc1c86b8bf1b
Henry Buckle <h.buckle@outlook.com>
2017-12-17 13:07:40 +0000

    debug

--- terraform-plan.sh
+++ terraform-plan.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 az login --msi
 TOKEN=$(curl http://localhost:50342/oauth2/token --data "resource=https://management.azure.com/" -H Metadata:true -s)
 echo "[$TOKEN]" > $HOME/.azure/accessTokens.json
+cat $HOME/.azure/accessTokens.json
 terraform init -input=false
 terraform plan -input=false -out=bkplan.tfplan
 buildkite-agent artifact upload bkplan.tfplan
