ba963730104362cf6dcf1d2a162fa909643ea81a
Henry Buckle <h.buckle@outlook.com>
2017-12-17 14:18:08 +0000

    set sub

--- terraform-init.sh
+++ terraform-init.sh
@@ -4,4 +4,6 @@ set -euo pipefail
 
 apk add terraform --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/edge/community
 az login --msi
+ID=az account show --query id
+az account set -s $ID
 terraform init -input=false
