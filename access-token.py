947570c888313b3a85b554dc243abf369250147f
Henry Buckle <h.buckle@outlook.com>
2017-12-17 16:43:27 +0000

    fix expires

--- access-token.py
+++ access-token.py
@@ -9,5 +9,7 @@ output["expiresIn"] = token["expires_in"]
 output["resource"] = "https://management.core.windows.net/"
 output["refreshToken"] = token["refresh_token"]
 output["accessToken"] = token["access_token"]
-output["expiresOn"] = datetime.fromtimestamp(int(token["expires_on"])).isoformat(' ', 'microseconds')
+#output["expiresOn"] = datetime.fromtimestamp(int(token["expires_on"])).isoformat(' ', 'microseconds')
+output["expiresOn"] = "2017-12-18 17:27:38.984891"
+#output["_authority"] = "https://login.microsoftonline.com"
 print(json.dumps(output))
