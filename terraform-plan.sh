ef1b3098de63a5b0282ae5aac738120dbb093f24
Henry Buckle <h.buckle@outlook.com>
2017-12-17 14:13:19 +0000

    get token from cli

--- terraform-plan.sh
+++ terraform-plan.sh
@@ -2,9 +2,7 @@
 
 set -euo pipefail
 
-TOKEN=$(curl http://localhost:50342/oauth2/token --data "resource=https://management.azure.com/" -H Metadata:true -s)
-echo $TOKEN | jq -c '[. | .["accessToken"] = .access_token | .["expiresOn"] = .expires_on | .["tokenType"] = .token_type | del(.access_token,.expires_on,.token_type) ]' > $HOME/.azure/accessTokens.json
-cat $HOME/.azure/accessTokens.json
+az account get-access-token | jq -c '[ .]' > $HOME/.azure/accessTokens.json
 terraform init -input=false
 terraform plan -input=false -out=bkplan.tfplan
 buildkite-agent artifact upload bkplan.tfplan
