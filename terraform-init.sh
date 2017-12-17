e1716faea4d236c77fb5649b584548e3d37a1c13
Henry Buckle <h.buckle@outlook.com>
2017-12-17 14:32:50 +0000

    get sub id

--- terraform-init.sh
+++ terraform-init.sh
@@ -3,4 +3,6 @@
 set -euo pipefail
 
 apk add terraform --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/edge/community
-terraform init -input=false
+ID=$(az login --msi | jq -r '.[].id')
+az account set -s $ID
+az account get-access-token | jq -c '[.]' > $HOME/.azure/accessTokens.json
