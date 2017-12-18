ffa343d4bd8592bf76c1f8464a5065aeda7406d3
Henry Buckle <h.buckle@outlook.com>
2017-12-18 09:20:58 +0000

    reinit

--- terraform-init.sh
+++ terraform-init.sh
@@ -2,10 +2,10 @@
 
 set -euo pipefail
 
-mkdir -p $TF_PLUGIN_CACHE_DIR
 apk add terraform --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/edge/community
 login=$(az login --msi)
 export ARM_TENANT_ID=$(echo $login | jq -r '.[].tenantId')
 export ARM_SUBSCRIPTION_ID=$(echo $login | jq -r '.[].id')
-curl -Lfs -o "$TF_PLUGIN_CACHE_DIR/terraform-provider-azurerm_v1.0.0_x4" https://temptf.blob.core.windows.net/temp/terraform-provider-azurerm_v1.0.0_x4
+mkdir -p ./terraform.d/plugins/linux_amd64
+curl -Lfs -o "./terraform.d/plugins/linux_amd64/terraform-provider-azurerm_v1.0.0_x4" https://temptf.blob.core.windows.net/temp/terraform-provider-azurerm_v1.0.0_x4
 terraform init -input=false
--- terraform-plan.sh
+++ terraform-plan.sh
@@ -2,6 +2,12 @@
 
 set -euo pipefail
 
+login=$(az login --msi)
+export ARM_TENANT_ID=$(echo $login | jq -r '.[].tenantId')
+export ARM_SUBSCRIPTION_ID=$(echo $login | jq -r '.[].id')
+mkdir -p ./terraform.d/plugins/linux_amd64
+curl -Lfs -o "./terraform.d/plugins/linux_amd64/terraform-provider-azurerm_v1.0.0_x4" https://temptf.blob.core.windows.net/temp/terraform-provider-azurerm_v1.0.0_x4
 terraform init -input=false
+
 terraform plan -input=false -out=bkplan.tfplan
 buildkite-agent artifact upload bkplan.tfplan
