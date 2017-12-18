154ea28088ae81bb1eb23b14c181c1d575198ce7
Henry Buckle <h.buckle@outlook.com>
2017-12-18 09:31:05 +0000

    chmod

--- terraform-run.sh
+++ terraform-run.sh
@@ -7,6 +7,7 @@ export ARM_TENANT_ID=$(echo $login | jq -r '.[].tenantId')
 export ARM_SUBSCRIPTION_ID=$(echo $login | jq -r '.[].id')
 mkdir -p ./terraform.d/plugins/linux_amd64
 curl -Lfs -o "./terraform.d/plugins/linux_amd64/terraform-provider-azurerm_v1.0.0_x4" https://temptf.blob.core.windows.net/temp/terraform-provider-azurerm_v1.0.0_x4
+chmod +x ./terraform.d/plugins/linux_amd64/terraform-provider-azurerm_v1.0.0_x4
 terraform init -input=false
 
 COMMAND=$(buildkite-agent meta-data get terraform-command)
