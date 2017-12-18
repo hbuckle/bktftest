25291ce9d0a61f26b12ec702d9152ca9cb40aae4
Henry Buckle <h.buckle@outlook.com>
2017-12-18 08:41:37 +0000

    update init

--- terraform-init.sh
+++ terraform-init.sh
@@ -5,8 +5,6 @@ set -euo pipefail
 apk add terraform --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/edge/community
 login=$(az login --msi)
 export ARM_TENANT_ID=$(echo $login | jq -r '.[].tenantId')
-export ARM_MSI_ENDPOINT=http://localhost:50342/oauth2/token
-export ARM_USE_MSI=true
 export ARM_SUBSCRIPTION_ID=$(echo $login | jq -r '.[].id')
-mkdir -p ./terraform.d/plugins/linux_amd64
-curl -Lfs -o ./terraform.d/plugins/linux_amd64/terraform-provider-azurerm_v1.0.0_x4 https://cdn-05.anonfile.com/c6F8nfd8ba/c197cb53-1513551501/terraform-provider-azurerm
+curl -Lfs -o "$TF_PLUGIN_CACHE_DIR/terraform-provider-azurerm_v1.0.0_x4" https://1drv.ms/u/s!Ag6hm31FMiVwjqoR5XuBYBWcXVjsOA
+terraform init -input=false
--- terraform-plan.sh
+++ terraform-plan.sh
@@ -2,6 +2,5 @@
 
 set -euo pipefail
 
-terraform init -input=false
 terraform plan -input=false -out=bkplan.tfplan
 buildkite-agent artifact upload bkplan.tfplan
