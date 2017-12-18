ff6aa85881fde2d93dbc369440a73d1c092a2da2
Henry Buckle <h.buckle@outlook.com>
2017-12-18 13:53:35 +0000

    get stor acc key

--- terraform-init.sh
+++ terraform-init.sh
@@ -9,4 +9,10 @@ export ARM_SUBSCRIPTION_ID=$(echo $login | jq -r '.[].id')
 mkdir -p ./terraform.d/plugins/linux_amd64
 curl -Lfs -o "./terraform.d/plugins/linux_amd64/terraform-provider-azurerm_v1.0.0_x4" https://temptf.blob.core.windows.net/temp/terraform-provider-azurerm_v1.0.0_x4
 chmod +x ./terraform.d/plugins/linux_amd64/terraform-provider-azurerm_v1.0.0_x4
-terraform init -input=false
+state_storage_access_key=$(
+  az storage account keys list \
+    --resource-group "subscription-terraform-state-northeurope" \
+    --account-name "qifoghtnvyauqtr" \
+    --query '[0].value' -o tsv
+)
+terraform init -input=false -backend-config="access_key=$state_storage_access_key"
