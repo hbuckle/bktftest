3a4ea89dec8c29250b11f700b27cac9198af1b66
Henry Buckle <h.buckle@outlook.com>
2017-12-18 09:01:11 +0000

    update link

--- terraform-init.sh
+++ terraform-init.sh
@@ -2,9 +2,10 @@
 
 set -euo pipefail
 
+mkdir -p $TF_PLUGIN_CACHE_DIR
 apk add terraform --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/edge/community
 login=$(az login --msi)
 export ARM_TENANT_ID=$(echo $login | jq -r '.[].tenantId')
 export ARM_SUBSCRIPTION_ID=$(echo $login | jq -r '.[].id')
-curl -Lfs -o "$TF_PLUGIN_CACHE_DIR/terraform-provider-azurerm_v1.0.0_x4" https://1drv.ms/u/s!Ag6hm31FMiVwjqoR5XuBYBWcXVjsOA
+curl -Lfs -o "$TF_PLUGIN_CACHE_DIR/terraform-provider-azurerm_v1.0.0_x4" https://temptf.blob.core.windows.net/temp/terraform-provider-azurerm_v1.0.0_x4
 terraform init -input=false
