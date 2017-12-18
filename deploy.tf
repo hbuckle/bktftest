9de58d221f131d6e55d0de0f7a1c4a2e7157c1de
Henry Buckle <h.buckle@outlook.com>
2017-12-18 13:40:23 +0000

    add backend

--- deploy.tf
+++ deploy.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "azurerm" {
+    storage_account_name = "qifoghtnvyauqtr"
+    container_name       = "tfstate"
+    key                  = "bktftest"
+  }
+}
+
 resource "azurerm_resource_group" "resource_group" {
   name     = "test"
   location = "northeurope"
